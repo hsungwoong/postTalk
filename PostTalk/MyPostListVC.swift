8c22d708bccfa1d21b97210efe7c62bf51164209
dykim <Home@dykim-macbook-air.local>
2015-06-15 16:22:05 +0900

    마이 포스트 목록

---
+++ PostTalk/MyPostListVC.swift
@@ -0,0 +1,35 @@
+//
+//  MyPostListVC.swift
+//  PostTalk
+//
+//  Created by dykim on 2015. 6. 15..
+//  Copyright (c) 2015년 cstone dev. All rights reserved.
+//
+
+import UIKit
+
+class MyPostListVC: CommonPostListVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
