4d797b9c2f4407b7e3c2f5c6ec7e36fd0a308f61
dykim <Home@dykim-macbook-air.local>
2015-06-23 11:32:05 +0900

    등록

--- PostTalk/PostInsertVC.swift
+++ PostTalk/PostInsertVC.swift
@@ -8,20 +8,163 @@
 
 import UIKit
 
-class PostInsertVC: BaseVC, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+class PostInsertVC: BaseVC, UIImagePickerControllerDelegate, UINavigationControllerDelegate , UITextViewDelegate {
     
+    @IBOutlet var bottomToolbar: UIToolbar!
+    @IBOutlet var myMemo: UITextView!
     @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
-    @IBOutlet var myMemo: UITextField!
+
     @IBOutlet var myImageView: UIImageView!
+    //@IBOutlet var keyboardHeight:NSLayoutConstraint!;
+    
+    let placeHolderText = "메세지를 입력해 주세요.";
+    
+    override var inputAccessoryView: UIView! {
+        get {
+            
+            var toolBar = UIView(frame: CGRectMake(0, 0, 200, 49))
+            var child = UIView(frame: CGRectMake(0, 0, 200, 49))
+            child.backgroundColor = UIColor.lightGrayColor()
+            toolBar.addSubview(child);
+            toolBar.layer.zPosition = 0;
+            
+/*
+            var toolBar = UIToolbar(frame: CGRectMake(0, 0, 200, 49));
+            toolBar.barStyle = UIBarStyle.BlackTranslucent;
+            
+            var arr = [AnyObject]();
+            
+            var bb = UIBarButtonItem(title: "test", style: UIBarButtonItemStyle.Plain, target: self, action: "gogo:");
+            arr.append(bb);
+            
+            toolBar.items = arr;
+            
+            //myMemo.inputAccessoryView = toolBar;
+
+*/
+            
+            return toolBar
+        }
+    }   /**/
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func loadView() {
+        NSBundle.mainBundle().loadNibNamed("PostInsertVC", owner: self, options: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //로그인페이지 호출
-        openLoginChk()
+        //openLoginChk()
+        
+        self.myMemo.delegate = self;
+
+        
+        if (myMemo.text == "") {
+            textViewDidEndEditing(myMemo)
+        }
+        
+        self.view.sendSubviewToBack(self.inputAccessoryView)
+        
+        //setupKbToolbar();
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillChangeFrameNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil);
+        
+        
+        self.reloadInputViews();
+        
+        println("########")
+        println(self.inputAccessoryView);
+        println(self.inputAccessoryView.superview);
+        
         
     }
     
+    override func canBecomeFirstResponder() -> Bool {
+        return true;
+    }
+    
+    func setupKbToolbar() {
+        
+        var toolBar = UIToolbar(frame: CGRectMake(0, 0, 200, 44));
+        toolBar.barStyle = UIBarStyle.BlackTranslucent;
+        
+        var arr = [AnyObject]();
+        
+        var bb = UIBarButtonItem(title: "test", style: UIBarButtonItemStyle.Plain, target: self, action: "gogo:");
+        arr.append(bb);
+        
+        toolBar.items = arr;
+        
+        myMemo.inputAccessoryView = toolBar;
+        
+        //myMemo.inputDelegate
+        //myMemo.inputAccessoryViewController
+        
+    }
+    
+    
+    
+    func gogo(target:AnyObject){
+        
+    }
+    
+    
+    
+    func keyboardWillShow(notification:NSNotification ) {
+        let info = notification.userInfo as NSDictionary?
+        let rectValue = info![UIKeyboardFrameBeginUserInfoKey] as! NSValue
+        let kbSize = rectValue.CGRectValue().size
+        
+        let contentInsets = UIEdgeInsetsMake(0, 0, kbSize.height - 45, 0)
+        myMemo.contentInset = contentInsets
+        myMemo.scrollIndicatorInsets = contentInsets;
+        
+        
+        // optionally scroll
+        var aRect = myMemo.superview!.frame
+        
+        aRect.size.height = aRect.size.height - CGFloat(kbSize.height + 45.0);
+        let targetRect = CGRectMake(0, 0, 10, 10) // get a relevant rect in the text view
+        if !aRect.contains(targetRect.origin) {
+            myMemo.scrollRectToVisible(targetRect, animated: true)
+        }
+
+/**/
+
+    }
+    
+    
+    
+    func keyboardWillHide(notification:NSNotification ) {
+        myMemo.contentInset = UIEdgeInsetsZero
+        myMemo.scrollIndicatorInsets = UIEdgeInsetsZero;
+    }
+    
+
+    func textViewDidBeginEditing(textView: UITextView) {
+        if (textView.text == placeHolderText ){
+            textView.text = ""
+            textView.textColor = UIColor.blackColor()
+        }
+        textView.becomeFirstResponder()
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if (textView.text == "") {
+            textView.text = placeHolderText;
+            textView.textColor = UIColor.lightGrayColor()
+        }
+        textView.resignFirstResponder()
+    }
+    
+
     //image album에서 선택
     @IBAction func selectPhotoButtonTapped(sender: AnyObject) {
         var myPickerController = UIImagePickerController()
@@ -36,14 +179,7 @@ class PostInsertVC: BaseVC, UIImagePickerControllerDelegate, UINavigationControl
         myImageUploadRequest()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func loadView() {
-        NSBundle.mainBundle().loadNibNamed("PostInsertVC", owner: self, options: nil)
-    }
+
     
     
     //고유 이미지 생성키
@@ -64,13 +200,13 @@ class PostInsertVC: BaseVC, UIImagePickerControllerDelegate, UINavigationControl
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         self.view.endEditing(true)
     }
-    
+    /*
     //UITextField Delegate
     func textFieldShouldReturn(textField: UITextField) -> Bool{
         textField.resignFirstResponder()
         return true
         
-    }
+    }*/
     
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
@@ -191,6 +327,11 @@ class PostInsertVC: BaseVC, UIImagePickerControllerDelegate, UINavigationControl
     
     func generateBoundaryString() -> String {
         return "Boundary-\(NSUUID().UUIDString)"
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "keyboardWillShow:", object: nil );
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "keyboardWillHide:", object: nil )
     }
     
 }
